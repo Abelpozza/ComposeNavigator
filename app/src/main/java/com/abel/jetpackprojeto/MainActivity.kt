157f97ce07e3ae0e104942adda0c493c292d7e20
abel <abelfloripa2000@gmail.com>
2026-01-25 12:42:45 -0300

    first commit

---
+++ app/src/main/java/com/abel/jetpackprojeto/MainActivity.kt
@@ -0,0 +1,72 @@
+package com.abel.jetpackprojeto
+
+import android.R
+import android.os.Bundle
+import android.provider.CalendarContract
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.abel.jetpackprojeto.ui.theme.JETPACKProjetoTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            JETPACKProjetoTheme {
+                Home()
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Home() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = "TopAppBar")
+                },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = Color.Red
+                )
+            )
+        }
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier
+                .padding(paddingValues)
+                .fillMaxSize()
+                .background(Color.Black),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(text = "Conteúdo da tela", color = Color.Green, fontSize = 30.sp)
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun HomePreview() {
+    JETPACKProjetoTheme {
+        Home()
+    }
+}
